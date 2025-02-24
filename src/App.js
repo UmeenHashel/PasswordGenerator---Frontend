b89ee61411f2fb9febbd0991460945556118bb89
UmeenHashel <umeenbg16@gmail.com>
2025-02-24 20:44:44 +0530

    Developed the initial structure of the frontend

--- src/App.js
+++ src/App.js
@@ -1,9 +1,58 @@
+import { useState } from "react";
 
 
 function App() {
+  const [length, setLength] = useState(50);
+  const [numbers, setNumbers] = useState(false);
+  const [symbols, setSymbols] = useState(false);
+  const [password, setPassword] = useState("");
+
+  const handleGeneratePassword = async () => {
+
+  };
+
   return (
-    <div>
-      <h1>Strong Password Generator</h1>
+    <div class="container">
+      <h1>Password Generator</h1>
+
+      <label>Password Length: </label>
+      <input
+        type="number"
+        value={length}
+        onChangeCapture={(e) => setLength(e.target.value)} 
+        min="4" max="50"/>
+
+      <div>
+        <label>
+          <input
+            type="checkbox"
+            checked={numbers}
+            onChange={() => setNumbers(!numbers)}
+          />
+          Include Numbers
+        </label>
+      </div>
+
+      <div>
+        <label>
+          <input
+            type="checkbox"
+            checked={symbols}
+            onChange={() => setSymbols(!symbols)}
+          />
+          Include Special Symbols
+        </label>
+      </div>
+
+      <button onClick={handleGeneratePassword}>Generate Password</button>
+
+      {password && (
+        <div>
+          <h2>Password</h2>
+          <div>{password}</div>
+        </div>
+      )}
+
     </div>
   );
 }
